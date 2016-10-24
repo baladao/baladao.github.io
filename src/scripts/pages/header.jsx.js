1f2c379d2195f2ba8d2054699b3899a85c299630
Baladão, Gustavo <x.baladao@gmail.com>
2016-10-24 16:15:51 -0200

    adding v1

---
+++ src/scripts/pages/header.jsx.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div className="ma4">
+        <span className="f3">Gustavo Vargas Baladão</span>
+      </div>
+    );
+  }
+};
+
+export default Header;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const DOCS_PATH = path.resolve(__dirname, './');
+const APP_PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+  context: APP_PATH,
+
+  entry: {
+    index: './scripts/main.jsx'
+  },
+
+  output: {
+    path: DOCS_PATH,
+    filename: '[name].js'
+  },
+
+  sassLoader: {
+    includePaths: [ 'client/style' ]
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.css$/, exclude: /node_modules/, loaders: ['style', 'css-loader', 'postcss'] },
+      { test: /\.ejs$/, exclude: /node_modules/, loader: 'ejs', query: { includePaths: [APP_PATH] } },
+      { test: /\.(eot|svg|ttf|woff|woff2)$/, exclude: /node_modules/, loader: 'file?name=fonts/[name].[ext]' },
+      { test: /\.[ot]tf$/, exclude: /node_modules/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=fonts/[name].[ext]' }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.ejs',
+      inject: false,
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true
+     }
+    }),
+
+    new CopyWebpackPlugin([{
+      from: 'manifest.json',
+    }]),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+};
