4305123a6c8a5c9b34475b98630a9d8d930e4739
baladao <6681705+baladao@users.noreply.github.com>
2022-03-02 20:31:31 +0000

    Deploying to gh-pages from  @ 44ba3cb9916198a09d102be7a281dad42297c0d2 🚀

--- _next/static/kDlwgl7I0ZTE8lCn4tlk5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/716-7a780d08a9e98297.js","static/chunks/pages/index-707131f642ed0ef2.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/s-fAxVlbLcrOx8GLhcnny/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/716-7a780d08a9e98297.js","static/chunks/pages/index-669f51312e416e40.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
