de0b98903470df93e39cd7addeb161701d78f708
Gustavo Baladao <gustavo.baladao@hp.com>
2022-03-02 17:20:01 -0300

    Fix styled components load

--- .next/static/fzu-jNqdJ210SUeg3jSNN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/928-8ba85fd7b5e948fd.js","static/chunks/pages/index-e9f05d3cf06fe25d.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -6,75 +6,73 @@ import { Link } from '../src/components/Link'
 
 export default () => {
   return (
-    <Container>
-      <Head>
-        <title>gustavo@baladao.dev</title>
-      </Head>
+    <GlobalStyles>
+      <Container>
+        <Main>
+          <Title>
+            contact: <a href="mailto:gustavo@baladao.dev">gustavo@baladao.dev</a>
+          </Title>
 
-      <Main>
-        <Title>
-          contact: <a href="mailto:gustavo@baladao.dev">gustavo@baladao.dev</a>
-        </Title>
+          <Grid>
+            <CardSection link="https://arctouch.com">
+              <h3>ArcTouch &rarr;</h3>
+              <Code>
+                Currently working on a project for <Link to="https://www.hp.com">HP</Link> using <Link to="https://single-spa.js.org">SSPA</Link>, <Link to="https://micro-frontends.org">MFEs</Link> and <Link to="https://reactjs.org">React</Link>.
+              </Code>
+            </CardSection>
 
-        <Grid>
-          <CardSection link="https://arctouch.com">
-            <h3>ArcTouch &rarr;</h3>
-            <Code>
-              Currently working on a project for <Link to="https://www.hp.com">HP</Link> using <Link to="https://single-spa.js.org">SSPA</Link>, <Link to="https://micro-frontends.org">MFEs</Link> and <Link to="https://reactjs.org">React</Link>.
-            </Code>
-          </CardSection>
+            <CardSection link="https://www.yieldstreet.com">
+              <h3>YieldStreet &rarr;</h3>
+              <Code>
+                As part of the Alternative Investments product the using <Link to="https://reactjs.org">React</Link> for Web and <Link to="https://reactnative.dev">React Native</Link> for Mobile.
+              </Code>
+            </CardSection>
 
-          <CardSection link="https://www.yieldstreet.com">
-            <h3>YieldStreet &rarr;</h3>
-            <Code>
-              As part of the Alternative Investments product the using <Link to="https://reactjs.org">React</Link> for Web and <Link to="https://reactnative.dev">React Native</Link> for Mobile.
-            </Code>
-          </CardSection>
+            <CardSection link="https://www.globo.com">
+              <h3>Globo.com &rarr;</h3>
+              <Code>
+                At <Link to="https://globoplay.com">globoplay</Link> developed the Web and Backend product using <Link to="https://reactjs.org">React</Link> and <Link to="https://www.apollographql.com">Apollo</Link> for Web and <Link to="https://nodejs.org">NodeJS</Link> and <Link to="https://www.apollographql.com">Apollo</Link> for the Backend.
+              </Code>
+            </CardSection>
 
-          <CardSection link="https://www.globo.com">
-            <h3>Globo.com &rarr;</h3>
-            <Code>
-              At <Link to="https://globoplay.com">globoplay</Link> developed the Web and Backend product using <Link to="https://reactjs.org">React</Link> and <Link to="https://www.apollographql.com">Apollo</Link> for Web and <Link to="https://nodejs.org">NodeJS</Link> and <Link to="https://www.apollographql.com">Apollo</Link> for the Backend.
-            </Code>
-          </CardSection>
-
-          <CardSection link="https://www.adp.com">
-            <h3>ADP Inc. &rarr;</h3>
-            <Code>
-              Worked at <Link to="https://expert.brasil.adp.com/instructions/instructions.html">Service Place</Link> using <Link to="https://reactjs.org">React</Link> for Web and <Link to="https://nodejs.org">NodeJS</Link> for the Backend.
-            </Code>
-          </CardSection>
-        </Grid>
-      </Main>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        a {
-          color: #0070f3;
-          text-decoration: none;      
-        }
-
-        a:hover,
-        a:focus,
-        a:active {
-          text-decoration: underline;
-        }
-      `}</style>
-    </Container>
+            <CardSection link="https://www.adp.com">
+              <h3>ADP Inc. &rarr;</h3>
+              <Code>
+                Worked at <Link to="https://expert.brasil.adp.com/instructions/instructions.html">Service Place</Link> using <Link to="https://reactjs.org">React</Link> for Web and <Link to="https://nodejs.org">NodeJS</Link> for the Backend.
+              </Code>
+            </CardSection>
+          </Grid>
+        </Main>
+      </Container>
+    </GlobalStyles>
   )
 }
+
+const GlobalStyles = styled.html`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+      sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    color: #0070f3;
+    text-decoration: none;      
+  }
+
+  a:hover,
+  a:focus,
+  a:active {
+    text-decoration: underline;
+  }
+`;
 
 const Container = styled.div`
   min-height: 100vh;
