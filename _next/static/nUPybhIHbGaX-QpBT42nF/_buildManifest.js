6cf173452ee5a38b1c897001e68bf013254a04f5
baladao <6681705+baladao@users.noreply.github.com>
2022-03-02 20:26:45 +0000

    Deploying to gh-pages from  @ de0b98903470df93e39cd7addeb161701d78f708 🚀

--- _next/static/nUPybhIHbGaX-QpBT42nF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/928-c7ae6ded79c1a84b.js","static/chunks/pages/index-e7730c6f7dc2a085.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
